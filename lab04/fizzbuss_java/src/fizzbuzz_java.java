ab7daa7f28d06f12f6db7ae6820244025b92be8e
JSIT-20 <jdeme051@uottawa.ca>
2021-06-09 14:40:38 -0400

    Refactored code, test 4 still works

--- lab04/fizzbuss_java/src/fizzbuzz_java.java
+++ lab04/fizzbuss_java/src/fizzbuzz_java.java
@@ -1,13 +1,12 @@
 class fizzbuzz_java{
 
   public static String fizzbuzz(int n){
-    if(n == 1 || n == 2 || n == 4){
-      return Integer.toString(n);
-    }
-    else if(n % 3 == 0){
+    if(n % 3 == 0){
       return "fizz";
     }
-    return "";
+    else{
+      return Integer.toString(n);
+    }
   }
 
 }
